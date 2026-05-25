d73912c2ff2ccd6ad46415aad08997e701222501
gab-cat <catimbanggabriel@gmail.com>
2026-05-25 18:51:11 +0800

    Update version to 1.1.1 and expand TAB_COUNT_CHOICES in actions.ts for more flexibility

--- src/bun/actions.ts
+++ src/bun/actions.ts
@@ -25,7 +25,7 @@ for (var i = 1; i <= steps; i++) {
 	await runShell(["osascript", "-l", "JavaScript", "-e", jxa]);
 }
 
-const TAB_COUNT_CHOICES = [2, 4, 6] as const;
+const TAB_COUNT_CHOICES = [1, 2, 3, 4, 5, 6] as const;
 
 async function actionCmdTab() {
 	await moveMouseRandomly();
